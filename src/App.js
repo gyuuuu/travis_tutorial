9ff53aa23a74f372ff34a8714c31a1a511491530
gyuuuu <kai9702@gmail.com>
2021-01-08 00:21:56 +0900

    test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          뉴로짱짱!
+          뉴로!
         </a>
       </header>
     </div>
--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/뉴로짱짱/i);
+  const linkElement = screen.getByText(/뉴로/i);
   expect(linkElement).toBeInTheDocument();
 });
